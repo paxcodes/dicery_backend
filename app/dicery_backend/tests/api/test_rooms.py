01d0d72697d195ffd73f4c439f73922e1cc75a6c
Margret `Pax` Williams <pax.margret@gmail.com>
2020-09-06 16:50:48 -0700

    :white_check_mark: Test /token endpoint on closed rooms

---
+++ app/dicery_backend/tests/api/test_rooms.py
@@ -0,0 +1,31 @@
+from typing import Generator
+
+from pytest import fixture, mark
+from sqlalchemy.orm import Session
+
+from dicery_backend import crud, schemas, models
+from starlette.testclient import TestClient
+
+
+class Test_when_room_is_unavailable:
+    """Tests that when room is marked as closed / "isAvailable" == False,
+    user will not be able to get an access token for it."""
+
+    @fixture
+    def givenClosedRoom(
+        self, db: Session
+    ) -> Generator[models.Room, None, None]:
+        roomSchema = schemas.RoomCreate(code="ABCDE1", owner="Pax")
+        room = crud.create_room(db, roomSchema)
+        crud.close_room(db, room_code=room.code)
+        yield room
+        crud.delete_room(db, room_code=room.code)
+
+    def test_players_cannot_receive_access_token_for_room(
+        self, client: TestClient, givenClosedRoom: models.Room
+    ):
+        response = client.post(
+            "/token", data={"room_code": givenClosedRoom.code, "player": "Sean"}
+        )
+        assert response.status_code == 404
+        assert response.json() == {"detail": "Room not found"}
--- app/dicery_backend/tests/conftest.py
+++ app/dicery_backend/tests/conftest.py
@@ -1,14 +1,22 @@
 from typing import Generator
 
 import debugpy
+from fastapi.testclient import TestClient
 from pytest import fixture
 
 from dicery_backend.database import SessionLocal
+from dicery_backend.main import app
 
 
 @fixture(scope="session")
 def db() -> Generator:
     yield SessionLocal()
+
+
+@fixture(scope="module")
+def client() -> Generator:
+    with TestClient(app) as c:
+        yield c
 
 
 @fixture(scope="session")
