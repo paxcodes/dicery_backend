cf36262965bbb012c00c1d0e0309348854154d90
Margret `Pax` Williams <pax.margret@gmail.com>
2020-10-01 06:24:34 -0700

    :whale: Use a separate docker-compose for staging env

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,15 @@
 # Exit in case of error
 set -e
 
+# If DICERY_ENV is empty
+if [ -z "$DICERY_ENV" ]
+then
+    export DOCKER_COMPOSE_FILE=docker-compose.yml
+else
+    export DOCKER_COMPOSE_FILE=docker-compose."${DICERY_ENV}".yml
+fi
+
 TAG=${TAG?TAG variable not set} \
 docker-compose \
--f docker-compose.yml \
+-f "${DOCKER_COMPOSE_FILE}" \
 build
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,12 +3,21 @@
 # Exit in case of error
 set -e
 
+# If DICERY_ENV is empty
+if [ -z "$DICERY_ENV" ]
+then
+    export DOCKER_COMPOSE_FILE=docker-compose.yml
+else
+    export DOCKER_COMPOSE_FILE=docker-compose."${DICERY_ENV}".yml
+fi
+
+
 DOMAIN=${DOMAIN?DOMAIN variable not set} \
 TRAEFIK_TAG=${TRAEFIK_TAG?TRAEFIK_TAG variable not set} \
 STACK_NAME=${STACK_NAME?STACK_NAME variable not set} \
 TAG=${TAG?TAG variable not set} \
 docker-compose \
--f docker-compose.yml \
+-f "${DOCKER_COMPOSE_FILE}" \
 config > docker-stack.yml
 
 docker-auto-labels docker-stack.yml
